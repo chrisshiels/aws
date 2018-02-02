e9c9f97595c02046bfebfbfe1787bfcbd15695c5
Chris Shiels <chris@systems-engineers.co.uk>
2018-02-02 20:41:14 +0000

    Added missing outputs for modules/aws/instanceprofile/.

--- terraform/modules/aws/instanceprofile/outputs.tf
+++ terraform/modules/aws/instanceprofile/outputs.tf
@@ -1,3 +1,11 @@
+output "role_name" {
+  value = "${aws_iam_role.role.name}"
+}
+
+output "policy_arn" {
+  value = "${aws_iam_policy.policy.arn}"
+}
+
 output "instance_profile_id" {
   value = "${aws_iam_instance_profile.instanceprofile.id}"
 }
