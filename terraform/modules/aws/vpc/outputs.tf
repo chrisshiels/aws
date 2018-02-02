f93766ee40a78e389fd2ad7c60032cb8fe96fd5f
Chris Shiels <chris@systems-engineers.co.uk>
2018-02-02 20:41:52 +0000

    Added missing outputs for modules/aws/vpc/.

--- terraform/modules/aws/vpc/outputs.tf
+++ terraform/modules/aws/vpc/outputs.tf
@@ -14,6 +14,18 @@ output "internet_gateway_id" {
   value = "${aws_internet_gateway.igw.id}"
 }
 
+output "eip_ids" {
+  value = "${aws_eip.nat.*.id}"
+}
+
 output "nat_gateway_ids" {
   value = "${aws_nat_gateway.nat.*.id}"
+}
+
+output "route_table_public_id" {
+  value = "${aws_route_table.public.id}"
+}
+
+output "route_table_private_ids" {
+  value = "${aws_route_table.private.*.id}"
 }
