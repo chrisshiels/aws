f024878b991727b086fd0a5e1ee86b0b9b0b4811
Chris Shiels <chris@systems-engineers.co.uk>
2018-02-26 09:08:50 +0000

    Refactored security groups in modules/aws/instance/.

--- terraform/modules/aws/instance/main.tf
+++ terraform/modules/aws/instance/main.tf
@@ -1,40 +1,21 @@
-resource "aws_security_group" "instance" {
-  name = "${format("sg_%s", replace(var.name, "-", "_"))}"
-  description = "sg-${var.name}"
+module "securitygroup" {
+  source = "../../../modules/aws/securitygroup"
+  name = "${var.name}"
   vpc_id = "${var.vpc_id}"
-
-  tags {
-    Name = "sg-${var.name}"
-  }
-}
-
-
-resource "aws_security_group_rule" "instance-egress-allall-to-all" {
-  security_group_id = "${aws_security_group.instance.id}"
-  type = "egress"
-  protocol = "all"
-  from_port = 0
-  to_port = 0
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "instance-egress-allall-to-all"
-}
-
-
-resource "aws_security_group_rule" "instance-ingress-tcp22-from-all" {
-  security_group_id = "${aws_security_group.instance.id}"
-  type = "ingress"
-  protocol = "tcp"
-  from_port = 22
-  to_port = 22
-  cidr_blocks = [ "0.0.0.0/0" ]
-  description = "instance-ingress-tcp22-from-all"
+  security_group_allow_cidrs_len = "${var.security_group_allow_cidrs_len}"
+  security_group_allow_cidrs = "${var.security_group_allow_cidrs}"
+  security_group_allow_ids_len = "${var.security_group_allow_ids_len}"
+  security_group_allow_ids = "${var.security_group_allow_ids}"
 }
 
 
 resource "aws_instance" "instance" {
   ami = "${var.ami_id}"
   instance_type = "${var.instance_type}"
-  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+  vpc_security_group_ids = [
+    "${var.security_group_ids}",
+    "${module.securitygroup.security_group_id}"
+  ]
   subnet_id = "${var.subnet_public_id}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
   key_name = "${var.key_name}"
--- terraform/modules/aws/instance/outputs.tf
+++ terraform/modules/aws/instance/outputs.tf
@@ -1,5 +1,5 @@
 output "security_group_id" {
-  value = "${aws_security_group.instance.id}"
+  value = "${module.securitygroup.security_group_id}"
 }
 
 output "instance_id" {
